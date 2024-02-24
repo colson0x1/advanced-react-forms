e478c8daea3d5c6bb3059d8289811fd3897fce0e
Colson <colsoncalifornication@gmail.com>
2024-02-24 13:07:58 +0545

    validate input upon lost focus blur

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,12 +6,27 @@ export default function Login() {
 
   // work with state using alternative combined state
   const [enteredValues, setEnteredValues] = useState({
-    email: '',
+    email: {
+      value: '',
+      didEdit: false,
+    },
+    // or We can merge the didBlur or didEdit on email too
+    /* email: {
+      value: '',
+      didEdit: false
+    }, */
     password: '',
   });
+  // or we can use extra state to manage didBlur
+  // here we're keeping track of whether the inputs have been edited which in
+  // this case simply means that they lost focus. so the user intracted with them
+  // and then they lost focus.
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
 
-  const emailIsInvalid =
-    enteredValues.email != '' && !enteredValues.email.includes('@');
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,6 +45,21 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    // using function form so that we don't lose any data
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      // dynamically target a property and set it to true because this
+      // handleInputBlur will be fired when an input is blurred which
+      // initially is false
+      [identifier]: true,
     }));
   }
 
@@ -54,6 +84,7 @@ export default function Login() {
             id='email'
             type='email'
             name='email'
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
